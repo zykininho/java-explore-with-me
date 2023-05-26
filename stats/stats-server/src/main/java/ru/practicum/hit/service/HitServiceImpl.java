3ae2e8562b047131c21c1ee4bb315455a36d17c9
MZykin <zykininho@gmail.com>
2023-05-26 13:32:14 +0600

    feat: реализация для тестов Postman

--- stats/stats-server/src/main/java/ru/practicum/hit/service/HitServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/hit/service/HitServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.practicum.hit.service;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import ru.practicum.hit.dto.EndpointHitDto;
 import ru.practicum.hit.mapper.HitMapper;
 import ru.practicum.hit.model.EndpointHit;
@@ -14,7 +15,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -24,7 +24,7 @@ public class HitServiceImpl implements HitService {
 
     private final HitRepository hitRepository;
     private final HitMapper hitMapper;
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedJdbcTemplate;
 
     @Override
     public EndpointHitDto addNewHit(EndpointHitDto endpointHitDto) {
@@ -59,42 +59,56 @@ public class HitServiceImpl implements HitService {
 
     private List<ViewStatsDto> getUniqueStatsFromStartToEndWithUris(LocalDateTime startDate, LocalDateTime endDate, List<String> uris) {
         log.info("Extracting unique stats from start={} to end={} and uri in ({})", startDate, endDate, uris);
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("start", startDate);
+        parameters.addValue("end", endDate);
+        parameters.addValue("uris", uris);
         String sql = "SELECT app, uri, COUNT(DISTINCT ip) AS hits\n" +
                 "FROM public.hits\n" +
-                "WHERE created_date >= ? AND created_date <= ?\tAND uri IN (?)\n" +
+                "WHERE created_date >= :start AND created_date <= :end AND uri IN (:uris)\n" +
                 "GROUP BY hits.app, hits.uri\n" +
                 "ORDER BY COUNT(DISTINCT ip) DESC";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> createViewStats(rs), startDate, endDate, uris);
+        return namedJdbcTemplate.query(sql, parameters, (rs, rowNum) -> createViewStats(rs));
     }
 
     private List<ViewStatsDto> getUniqueStatsFromStartToEnd(LocalDateTime startDate, LocalDateTime endDate) {
         log.info("Extracting unique stats from start={} to end={}", startDate, endDate);
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("start", startDate);
+        parameters.addValue("end", endDate);
         String sql = "SELECT app, uri, COUNT(DISTINCT ip) AS hits\n" +
                 "FROM public.hits\n" +
-                "WHERE created_date >= ? AND created_date <= ?\n" +
+                "WHERE created_date >= :start AND created_date <= :end\n" +
                 "GROUP BY hits.app, hits.uri\n" +
                 "ORDER BY COUNT(DISTINCT ip) DESC";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> createViewStats(rs), startDate, endDate);
+        return namedJdbcTemplate.query(sql, parameters, (rs, rowNum) -> createViewStats(rs));
     }
 
     private List<ViewStatsDto> getStatsFromStartToEndWithUris(LocalDateTime startDate, LocalDateTime endDate, List<String> uris) {
         log.info("Extracting all stats from start={} to end={} and uri in ({})", startDate, endDate, uris);
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("start", startDate);
+        parameters.addValue("end", endDate);
+        parameters.addValue("uris", uris);
         String sql = "SELECT app, uri, COUNT(ip) AS hits\n" +
                 "FROM public.hits\n" +
-                "WHERE created_date >= ? AND created_date <= ?\tAND uri IN (?)\n" +
+                "WHERE created_date >= :start AND created_date <= :end AND uri IN (:uris)\n" +
                 "GROUP BY hits.app, hits.uri\n" +
                 "ORDER BY COUNT(ip) DESC";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> createViewStats(rs), startDate, endDate, uris);
+        return namedJdbcTemplate.query(sql, parameters, (rs, rowNum) -> createViewStats(rs));
     }
 
     private List<ViewStatsDto> getStatsFromStartToEnd(LocalDateTime startDate, LocalDateTime endDate) {
         log.info("Extracting all stats from start={} to end={}", startDate, endDate);
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("start", startDate);
+        parameters.addValue("end", endDate);
         String sql = "SELECT app, uri, COUNT(ip) AS hits\n" +
                 "FROM public.hits\n" +
-                "WHERE created_date >= ? AND created_date <= ?\n" +
+                "WHERE created_date >= :start AND created_date <= :end\n" +
                 "GROUP BY hits.app, hits.uri\n" +
                 "ORDER BY COUNT(ip) DESC";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> createViewStats(rs), startDate, endDate);
+        return namedJdbcTemplate.query(sql, parameters, (rs, rowNum) -> createViewStats(rs));
     }
 
     private ViewStatsDto createViewStats(ResultSet rs) throws SQLException {
