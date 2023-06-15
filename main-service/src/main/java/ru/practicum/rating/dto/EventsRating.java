5df0fd10291a3ea17667d6753a99f59b8f0a25ec
MZykin <zykininho@gmail.com>
2023-06-15 13:59:58 +0600

    feat: добавлена модель для ответа на запрос установки like/dislike событиям

--- main-service/src/main/java/ru/practicum/rating/dto/EventsRating.java
+++ main-service/src/main/java/ru/practicum/rating/dto/EventsRating.java
@@ -3,10 +3,13 @@ package ru.practicum.rating.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 public class EventsRating {
 
-
+    private List<Long> eventIds;
+    private String rating;
 
 }
